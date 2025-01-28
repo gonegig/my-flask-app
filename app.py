0274f8fecbecd225a8a2ff91a44425312a545660
gonegig <138560038+gonegig@users.noreply.github.com>
2025-01-28 15:28:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Blue-green deployment"
+    return "Blue-green deployment-newpipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
